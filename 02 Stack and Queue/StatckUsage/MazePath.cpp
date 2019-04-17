8ca5d1920ec949b8d0b4e0ac1dea2d9737a8f1f3
KianGao <1207529462@qq.com>
2019-04-17 18:50:50 +0800

    stack maze

--- 02 Stack and Queue/StatckUsage/MazePath.cpp
+++ 02 Stack and Queue/StatckUsage/MazePath.cpp
@@ -6,64 +6,136 @@ using namespace std;
 // 利用栈寻找迷宫路径
 
 // 使用二维数组表示迷宫
-// 起点为(1,1)，终点为(9,9)
-#define size 10
-int maze[size][size] = {
-    0,0,0,0,0,0,0,0,0,0,
-    0,1,1,0,1,1,1,0,1,0,
-    0,1,1,0,1,1,1,0,1,0,
-    0,1,1,1,1,0,0,1,1,0,
-    0,1,0,0,0,1,1,1,1,0,
-    0,1,1,1,0,1,1,1,1,0,
-    0,1,0,1,1,1,0,1,1,0,
-    0,1,0,0,0,1,0,0,1,0,
-    0,0,1,1,1,1,1,1,1,0,
-    0,0,0,0,0,0,0,0,0,0
+// 起点为(1,1)，终点为(8,8)
+#define SIZE 10
+#define MAX_ROW 10
+#define MAX_COL 10
+int maze[SIZE][SIZE] = {
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 0, 0, 1, 0, 0, 0, 1, 0, 1,
+    1, 0, 0, 1, 0, 0, 0, 1, 0, 1,
+    1, 0, 0, 0, 0, 1, 1, 0, 0, 1,
+    1, 0, 1, 1, 1, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 1, 0, 0, 0, 0, 1,
+    1, 0, 1, 0, 0, 0, 1, 0, 0, 1,
+    1, 0, 1, 1, 1, 0, 1, 1, 0, 1,
+    1, 1, 0, 0, 0, 0, 1, 0, 0, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+
+struct Node
+{
+    int row;
+    int col;
 };
 
-struct Node{
-    int x;int y;
-    int dir;
-};
+void print_line()
+{
+    int i, j;
+    for (i = 0; i < MAX_ROW; i++)
+    {
+        for (j = 0; j < MAX_COL; j++)
+            if (maze[i][j] == 1)
+            {
+                cout.width(4);
+                cout << "*";
+            }
+            else if (maze[i][j] >= 3)
+            {
+                cout.width(4);
+                cout<<maze[i][j]-2;
+            }
+            else
+            {
+                cout.width(4);
+                cout << "-";
+            }
+        cout << endl;
+    }
+}
+
+void visit(Node p, int sign, stack<Node> &S)
+{
+    // visit()函数将经过的地方设为2
+    // 压入当前所在结点，以及到达的结点
+    S.push(p);
+    switch (sign)
+    {
+    case 1:
+        p.col++;
+        S.push(p);
+        maze[p.row][p.col] = 2;
+        break; //向右
+    case 2:
+        p.row++;
+        S.push(p);
+        maze[p.row][p.col] = 2;
+        break; //向下
+    case 3:
+        p.col--;
+        S.push(p);
+        maze[p.row][p.col] = 2;
+        break; //向左
+    case 4:
+        p.row--;
+        S.push(p);
+        maze[p.row][p.col] = 2;
+        break; //向上
+    }
+}
 
 int main()
 {
-    stack<Node> s;
-    int x = 1;
-    int y = 1;
-    int dir = 0; //dir表示下一步的方向，0~3分别为上右下左
-    Node curNode = Node{x, y, dir};
-    do
+    print_line();
+    Node p = Node{1, 1};
+    maze[p.row][p.col] = 2; //遍历过的点设置为2
+    stack<Node> S;
+    S.push(p);
+    while (!S.empty())
     {
-        if(maze[x][y] == 1){
-            int nextX,nextY;
-            switch (dir)
-            {
-                case 0:
-                    nextX = x;
-                    nextY = y-1;
-                    break;
-                case 1:
-                    nextX = x+1;
-                    nextY = y;
-                    break;
-                case 2:
-                    nextX = x;
-                    nextY = y+1;
-                    break;
-                case 3:
-                    nextX = x-1;
-                    nextY = y;
-                    break;
-                case 4:
-                    Node popNode = s.top();
-                    s.pop();
-                    maze[x][y] = 0;
-                    x = popNode.x; y = popNode.y;
-                    maze[x][y] = 1;
-            }
+        p = S.top();
+        S.pop();
+        cout<<p.row<<" "<<p.col<<endl;
+        if (p.row == MAX_ROW - 2 && p.col == MAX_COL - 2)
+            break;
+        if (p.col + 1 < MAX_COL - 1 && maze[p.row][p.col + 1] == 0) //向右
+        {
+            visit(p, 1, S);
+            continue;
         }
-    } while (!s.empty());
-    cout<<"No solution."<<endl;
+        if (p.row + 1 < MAX_ROW - 1 && maze[p.row + 1][p.col] == 0) //向下
+        {
+            visit(p, 2, S);
+            continue;
+        }
+        if (p.col - 1 >= 1 && maze[p.row][p.col - 1] == 0) //向左
+        {
+            visit(p, 3, S);
+            continue;
+        }
+        if (p.row - 1 >= 1 && maze[p.row - 1][p.col] == 0) //向上
+        {
+            visit(p, 4, S);
+            continue;
+        } //以上是对迷宫的四个方向进行操作
+    }
+    if (p.row == MAX_ROW - 2 && p.col == MAX_COL - 2) //是否为出口
+    {
+        int count = S.size() + 3; //为了与迷宫0,1,2的区别所以基数要以3开始
+        cout << "(" << p.row << "," << p.col << ")" << endl;
+        maze[p.row][p.col] = count;
+        while (!S.empty()) //按照前驱进行查找
+        {
+            count--;
+            p = S.top();
+            S.pop();
+            maze[p.row][p.col] = count;
+            cout << "(" << p.row << "," << p.col << ")" << endl;
+        }
+        print_line();
+    }
+    else
+    {
+        cout<<"没有出路"<<endl;
+    }
     return 0;
 }
